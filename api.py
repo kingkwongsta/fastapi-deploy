48357ac120cf81568cdc2fb7b4d1506a13a1e9de
Kwongster <bryant.j.kwong@gmail.com>
2024-03-07 17:12:35 -0800

    Add new endpoint for generating cocktail recipe

--- api.py
+++ api.py
@@ -13,3 +13,14 @@ async def testing():
 @app.get("/more")
 async def more():
     return {"1 + 1 = 3"}
+
+from cocktail import generate_cocktail_recipe
+user_liquor = "Whiskey"
+user_flavor = "Spicy"
+user_mood = "Relaxed"
+
+@app.get("/cocktail")
+# async def get_cocktail(liquor: str = Query(default=None), flavor: str = Query(default=None), mood: str = Query(default=None)):
+#     return generate_cocktail_recipe(liquor, flavor, mood)
+async def get_cocktail():
+    return generate_cocktail_recipe(user_liquor, user_flavor, user_mood)
