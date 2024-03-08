dfbd076c9792cf8760b46a18b9de0c75f37f27b3
Kwongster <bryant.j.kwong@gmail.com>
2024-03-07 17:09:41 -0800

    Add import of 'app' from 'api' for uvicorn run

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 
 import uvicorn
 from os import getenv
+from api import app
 
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
-    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
